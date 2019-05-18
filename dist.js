7b61ca1bd95e689f0ef68c30c7ad9a27c951a069
Tyler Deitz <tylerdeitz@gmail.com>
2019-05-18 11:10:45 -0700

    up dev deps

--- dist.js
+++ dist.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = exports.composeDerivedStateFromProps = exports.composeState = void 0;
+exports["default"] = exports.composeDerivedStateFromProps = exports.composeState = void 0;
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
 
@@ -32,4 +32,4 @@ exports.composeState = composeState;
 var composeDerivedStateFromProps = composeStateFactory(1);
 exports.composeDerivedStateFromProps = composeDerivedStateFromProps;
 var _default = composeState;
-exports.default = _default;
+exports["default"] = _default;
