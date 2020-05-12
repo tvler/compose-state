19ed56aaedaf1781fa9f8a99f931809456d4bc3d
Tyler Deitz <tylerdeitz@gmail.com>
2020-05-12 10:52:23 -0700

    fix security

--- dist.js
+++ dist.js
@@ -7,7 +7,7 @@ exports["default"] = exports.composeDerivedStateFromProps = exports.composeState
 
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
@@ -20,8 +20,8 @@ var composeStateFactory = function composeStateFactory(stateIndex) {
     return updaters.reduceRight(function (accumulator, current) {
       return function (stateOrProps1, stateOrProps2) {
         var accumulatedState = accumulator(stateOrProps1, stateOrProps2);
-        var currentState = current instanceof Function ? stateIndex === 0 ? current(_objectSpread({}, stateOrProps1, {}, accumulatedState), stateOrProps2) : current(stateOrProps1, _objectSpread({}, stateOrProps2, {}, accumulatedState)) : current;
-        return currentState || accumulatedState ? _objectSpread({}, accumulatedState, {}, currentState) : null;
+        var currentState = current instanceof Function ? stateIndex === 0 ? current(_objectSpread(_objectSpread({}, stateOrProps1), accumulatedState), stateOrProps2) : current(stateOrProps1, _objectSpread(_objectSpread({}, stateOrProps2), accumulatedState)) : current;
+        return currentState || accumulatedState ? _objectSpread(_objectSpread({}, accumulatedState), currentState) : null;
       };
     }, function () {
       return null;
